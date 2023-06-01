d6689a3e2d8c95265dfe6f319cc17ce26b33133b
danipetro21 <idany346@gmail.com>
2023-06-01 14:29:54 +0200

    snack 8

--- src/org/learning/java/snacks/Snack8.java
+++ src/org/learning/java/snacks/Snack8.java
@@ -1,2 +1,38 @@
-package org.learning.java.snacks;public class Snack8 {
+package org.learning.java.snacks;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Snack8 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("quante cifre vuoi inserire?: ");
+        int n = scan.nextInt();
+
+        boolean valid = false;
+        int num = 0;
+        int molt = 1;
+        int sum = 0;
+        //int[] rest = new int[n];
+
+        do {
+            System.out.println("inserisci un numero di " + n + " cifre");
+            num = scan.nextInt();
+
+            if (num > 0) {
+                valid = true;
+            }
+
+        } while (!valid);
+
+        for (int i = 0; i < n; i++) {
+            sum += (num / molt) % 10 ;
+            molt *= 10;
+        }
+
+        System.out.println("La somma di ogni cifra è: " + sum);
+
+    }
+
 }
